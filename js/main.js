ad6753e1ebd243bf138f1e1ca904cf9792c5b60c
Gaye Lowenstein <glowen@hotmail.com>
2016-10-03 10:28:36 -0700

    Gaye's TicTacToe unfinished

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var player1 = 'X';
+var player2 = 'O';
+var xoro = document.getElementByClassName('xoro')
+
+for (vari = 0; i < xoro.length; i++) {
+  xoro[i].addEventListener('click', function() {
+    var changexo;
+    if(this.id === 'player1') {
+      changexo = X
+    } else {
+      (this.id === 'player2') {
+      changexo = O
+    }
+    document.getElementByTagName()
+  })
+}
+
+document.getElementById('player1').addEventListener('click', function() {
+  var xoro = document.getElementByClassName('xoro');
+  if(this.id === 'player1') {
+    xoro = X
+  } else if {
+    (this.id === 'player2') {
+    xoro = O
+  }
+}}
+
+  document.getElementByClassName('xoro');
+})
